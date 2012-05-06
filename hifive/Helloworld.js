b94ca34167354c69d2c6e8d6796bf6502081c3ff
michitakaiida <michitaka.iida@gmail.com>
2012-05-06 13:12:37 +0900

    hifiveFirst

---
+++ hifive/Helloworld.js
@@ -0,0 +1,12 @@
+
+$(function() {  
+    var helloWorldController = {  
+        __name: 'HelloWorldController',  
+   
+        '#btn click': function() {  
+            alert('Hello, World!');  
+        }  
+    };  
+   
+    h5.core.controller('#container', helloWorldController );  
+});  
